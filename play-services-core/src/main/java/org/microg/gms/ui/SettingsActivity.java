6cc9d04a53634400d624400d29bf8dab126a2b8f
KAZI MMT <82371061+kazimmt@users.noreply.github.com>
2023-06-06 23:46:17 +0600

    apply DynamicColors

--- play-services-core/src/main/java/org/microg/gms/ui/SettingsActivity.java
+++ play-services-core/src/main/java/org/microg/gms/ui/SettingsActivity.java
@@ -1,6 +1,7 @@
 package org.microg.gms.ui;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -10,13 +11,16 @@ import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.google.android.material.color.DynamicColors;
 import com.google.android.gms.R;
+
+import java.util.Objects;
 
 public class SettingsActivity extends AppCompatActivity {
     private AppBarConfiguration appBarConfiguration;
 
     private NavController getNavController() {
-        return ((NavHostFragment)getSupportFragmentManager().findFragmentById(R.id.navhost)).getNavController();
+        return ((NavHostFragment) Objects.requireNonNull(getSupportFragmentManager().findFragmentById(R.id.navhost))).getNavController();
     }
 
     @Override
@@ -27,6 +31,10 @@ public class SettingsActivity extends AppCompatActivity {
         NearbyPreferencesIntegration.Companion.preProcessSettingsIntent(intent);
 
         setContentView(R.layout.settings_root_activity);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            DynamicColors.applyToActivityIfAvailable(this);
+        }
 
         appBarConfiguration = new AppBarConfiguration.Builder(getNavController().getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, getNavController(), appBarConfiguration);
